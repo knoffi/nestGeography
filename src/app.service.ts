76431ef0efb3bb7bf8de1b38bb6304cfb404b9dc
knoffi <matthiastutor@outlook.de>
2022-02-04 23:35:17 +0100

    ADD #1 response type from service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
+export type HelloWorld = { message: string };
 @Injectable()
 export class AppService {
-    getHello() {
+    getHello(): HelloWorld {
         return { message: 'Hello My Dear World!' };
     }
 }
