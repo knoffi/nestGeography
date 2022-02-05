2f4e53119ffcc7e1dee8a05f0bb8a75ce34693b6
knoffi <matthiastutor@outlook.de>
2022-02-05 18:22:51 +0100

    ADD #2 get pokemon name by id param

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,9 +5,9 @@ import { map } from 'rxjs/operators';
 @Injectable()
 export class AppService {
     constructor(private httpService: HttpService) {}
-    getPokeAttack() {
+    getPokeAttack(id: string) {
         return this.httpService
-            .get('https://pokeapi.co/api/v2/pokemon/25')
+            .get('https://pokeapi.co/api/v2/pokemon/' + id)
             .pipe(map((res) => res.data.name));
     }
 }
