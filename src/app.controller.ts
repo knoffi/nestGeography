17a32cd1c7942d471e99bdeddc909c05e9f60c25
knoffi <matthiastutor@outlook.de>
2022-02-06 18:40:04 +0100

    ADD #2 respond with random attack from all moves

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService, NameHolder } from './app.service';
 
 @Controller()
 export class AppController {
+    private static readonly MAX_POKEMON_ID = 10220;
     constructor(private readonly appService: AppService) {}
-
     @Get(':id')
-    getPokeName(@Param() params): any {
-        return this.appService.getPokeAttack(params.id);
+    async getPokeName(@Param() params): Promise<NameHolder> {
+        if (this.testPokemonId(params.id)) {
+            return await this.appService.getPokeAttack(params.id);
+        }
+        return {
+            name:
+                'No valid pokemon id! Id needs to be an integer between 1 and ' +
+                AppController.MAX_POKEMON_ID,
+            url: '',
+        };
+    }
+
+    testPokemonId(id: string): boolean {
+        return true;
     }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,39 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { map } from 'rxjs/operators';
-type NameHolder = { name: string; url: string };
+export type NameHolder = { name: string; url: string };
 type MoveHolder = { move: NameHolder };
 type Moves = { moves: Array<MoveHolder> };
 @Injectable()
 export class AppService {
     constructor(private httpService: HttpService) {}
-    getPokeAttack(id: string): Observable<{ name: string }> {
-        const randomIndex = Math.floor(Math.random() * 4);
-        const moveNames = this.httpService
-            .get<Moves>('https://pokeapi.co/api/v2/pokemon/' + id)
-            .pipe(map((res) => res.data.moves[randomIndex].move));
-        return moveNames;
+    private static readonly DEFAULT_MOVE = {
+        move: {
+            name: 'struggle',
+            url: 'https://pokeapi.co/api/v2/move/struggle/',
+        },
+    };
+
+    async getPokeAttack(id: string): Promise<NameHolder> {
+        try {
+            const moveNames = await firstValueFrom(
+                this.httpService
+                    .get<Moves>('https://pokeapi.co/api/v2/pokemon/' + id)
+                    .pipe(map((res) => res.data.moves))
+            );
+            return this.randomEntry(moveNames, AppService.DEFAULT_MOVE).move;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    randomEntry<T>(array: Array<T>, defaultValue: T): T {
+        if (array.length === 0) {
+            return defaultValue;
+        } else {
+            const randomIndex = Math.floor(Math.random() * array.length);
+            const randomEntry = array[randomIndex];
+            return randomEntry;
+        }
     }
 }
