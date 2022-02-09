b25b9e4642de6e2e29494003aadc7e32833579d9
knoffi <matthiastutor@outlook.de>
2022-02-09 23:01:11 +0100

    REFACTOR #12 poke test by testing module

--- src/pokemon/poke.controller.spec.ts
+++ src/pokemon/poke.controller.spec.ts
@@ -1,15 +1,25 @@
+import { Test } from '@nestjs/testing';
 import { PokeController } from './poke.controller';
+import { IPokeService } from './poke.service';
 import { PokeServiceMock } from './poke.service.mock';
 
 // needs in package.json:   "jest:": {"restoreMocks":true}, otherwise reset spys with beforeEach(...)
 describe('PokeController', () => {
-    const service = new PokeServiceMock();
-    const controller = new PokeController(service);
-    describe('response', () => {
-        it('valid id', async () => {
-            const body = await controller.getPokeName({ id: 150 });
-            expect(body).toHaveProperty('name');
-            expect(body).toHaveProperty('url');
-        });
+    let controller: PokeController;
+    let service: IPokeService;
+
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            controllers: [PokeController],
+            providers: [{ useClass: PokeServiceMock, provide: 'IPokeService' }],
+        }).compile();
+
+        service = moduleRef.get<IPokeService>('IPokeService');
+        controller = moduleRef.get<PokeController>(PokeController);
+    });
+    it('response from valid id', async () => {
+        const body = await controller.getPokeName({ id: 150 });
+        expect(body).toHaveProperty('name');
+        expect(body).toHaveProperty('url');
     });
 });
