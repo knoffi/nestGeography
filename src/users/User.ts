4ced8cf499f5e571e63f2bf2a946cdca0436cb22
knoffi <matthiastutor@outlook.de>
2022-03-15 14:55:35 +0100

    ADD #14 fourth stub user

--- src/users/User.ts
+++ src/users/User.ts
@@ -19,4 +19,5 @@ export const users: User[] = [
     new User(69, 'Barney Stinson', 'awesome@gnb.com'),
     new User(42, 'Hitchhiker', 'through@the-galaxy.com'),
     new User(17, 'Michael Spivak', 'calculus@analysis.com'),
+    new User(123, 'Max Mustermann', 'mymail@service.com'),
 ];
